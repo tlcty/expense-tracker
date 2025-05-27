ff54257085789d9a54c031186f9200c6a3c33b23
finik <egor.tolstoy.s@gmail.com>
2025-05-27 20:06:36 +0300

    обновил логику и сервер

--- server/app.py
+++ server/app.py
@@ -1,9 +1,36 @@
-# server/app.py
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from logic.models import Expense
+from logic.services import ExpenseTracker
 
 app = FastAPI()
-
 @app.get("/")
 def index():
-    return {"message": "Expense Tracker API is running."}
+    return {"message": "ну вроде работает :) ."}
+tracker = ExpenseTracker()
+
+class ExpenseIn(BaseModel):
+    title: str
+    category: str
+    amount: float
+    date: str  # формат "дд.мм"
+
+@app.post("/expenses")
+def add_expense(exp: ExpenseIn):
+    tracker.add_expense(Expense(exp.title, exp.category, exp.amount, exp.date))
+    return {"status": "ok"}
+
+@app.get("/top-category")
+def get_top_category(month: str):
+    result = tracker.top_category(month)
+    return {"category": result[0], "total": result[1]} if result else {"message": "Нет данных"}
+
+@app.get("/max-expense")
+def get_max_expense(month: str, category: str = None):
+    e = tracker.max_expense(month, category)
+    return {
+        "title": e.title,
+        "category": e.category,
+        "amount": e.amount,
+        "date": e.date.strftime("%d.%m")
+    } if e else {"message": "Нет данных"}
